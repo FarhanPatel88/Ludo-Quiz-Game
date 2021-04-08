0a8fe39b66b6906779f52217eb61f86c34575655
Farhan Patel <farhanp9876@gmail.com>
2021-04-08 13:28:32 +0530

    Get rid of mongoose-auto-increment for now

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const autoIncrement = require('mongoose-auto-increment')
+// const autoIncrement = require('mongoose-auto-increment')
 
 let uri = 'mongodb://localhost:27017/ludo_game'
 
